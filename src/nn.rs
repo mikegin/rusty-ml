ba5f7caa7569936645ad75eafe1eeee6bf52f87c
Mike Gindin <gindin.mike@gmail.com>
2023-05-04 16:14:12 -0400

    wip matrix stuff

--- src/nn.rs
+++ src/nn.rs
@@ -21,51 +21,35 @@ fn transform_a_to_v_matrix(a: [[f64; 3]; 4]) -> Vec<Vec<f64>> {
     v
 }
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct Matrix {
-    rows: Vec<f32>,
-    cols: Vec<f32>
+    inner: Vec<Vec<f32>>
 }
 
 impl Matrix {
-    pub fn sum(&mut self, other: &Matrix) {
-        for i in 0..self.rows.len() {
-            for j in 0..self.cols.len() {
-                let other_i = match other.rows.get(i) {
-                    Some(x) => *x,
-                    None => 0.
-                };
-                let other_j = match other.cols.get(j) {
-                    Some(x) => *x,
-                    None => 0.
-                };
-                
-                self.rows.insert(i, *self.rows.get(i).unwrap() + other_i);
-                self.cols.insert(j, *self.cols.get(j).unwrap() + other_j);
+    pub fn new() -> Self {
+       Self {
+        inner: vec![vec![]]
+       }
+    }
 
+    pub fn sum(&mut self, other: &Matrix) {
+        assert_eq!(self.inner.len(), other.inner.len());
+        assert!(self.inner.len() > 0);
+        assert_eq!(self.inner.get(0).unwrap().len(), other.inner.get(0).unwrap().len());
+        assert!(self.inner.get(0).unwrap().len() > 0);
+
+        for (i, row) in self.inner.iter_mut().enumerate() {
+            for (j, col) in row.iter_mut().enumerate() {
+                *col += other.inner
+                                    .get(i)
+                                    .unwrap()
+                                    .get(j)
+                                    .unwrap();
             }
         }
     }
     pub fn dot(&mut self, other: &Matrix) {
-        for i in 0..self.rows.len() {
-            for j in 0..self.cols.len() {
-                let other_i = match other.rows.get(i) {
-                    Some(x) => *x,
-                    None => 0.
-                };
-                let other_j = match other.cols.get(j) {
-                    Some(x) => *x,
-                    None => 0.
-                };
-
-                let self_i = self.rows.get(i).unwrap();
-                let self_j = self.cols.get(j).unwrap();
-                
-                self.rows.insert(i, self_i + *self_i * other_i);
-                self.cols.insert(j, *self_j * other_j);
-
-            }
-        }
     }
 }
 
@@ -87,22 +71,26 @@ mod tests {
 
     #[test]
     fn matrix_sum() {
-        let mut m1 = Matrix::default();
-        m1.rows.push(1.);
-        m1.cols.push(2.);
-
+        let mut m1 = Matrix::new();
+        m1.inner.get_mut(0).unwrap().push(1.);
+        m1.inner.get_mut(0).unwrap().push(2.);
         
-        let mut m2 = Matrix::default();
-        m2.rows.push(3.);
-        m2.cols.push(4.);
+        let mut m2 = Matrix::new();
+        m2.inner.get_mut(0).unwrap().push(3.);
+        m2.inner.get_mut(0).unwrap().push(4.);
 
         m1.sum(&m2);
 
-        assert_eq!(*m1.rows.get(0).unwrap(), 4.);
-        assert_eq!(*m1.cols.get(0).unwrap(), 6.);
+        assert_eq!(*m1.inner.get(0).unwrap().get(0).unwrap(), 4.);
+        assert_eq!(*m1.inner.get(0).unwrap().get(1).unwrap(), 6.);
 
-        assert_eq!(*m2.rows.get(0).unwrap(), 3.);
-        assert_eq!(*m2.cols.get(0).unwrap(), 4.);
+        assert_eq!(*m2.inner.get(0).unwrap().get(0).unwrap(), 3.);
+        assert_eq!(*m2.inner.get(0).unwrap().get(1).unwrap(), 4.);
+    }
+
+    #[test]
+    fn matrix_dot() {
+
     }
 
 
